477b18b0b67ea4586ef03caaf32e54efcac412f0
Duncan Thomson <dthomson@esri.com>
2024-06-03 22:33:01 +0100

    Add conanfile_rtc.py

---
+++ conanfile_rtc.py
@@ -0,0 +1,24 @@
+from conans import ConanFile
+
+class DawnConan(ConanFile):
+    name = "dawn"
+    version = "0.0.1"
+    url = "https://devtopia.esri.com/dunc8812/dawn"
+    license = "https://devtopia.esri.com/dunc8812/dawn/blob/main/LICENSE"
+    description = "Dawn is an open-source and cross-platform implementation of the WebGPU standard."
+
+    # RTC specific triple
+    settings = "platform_architecture_target"
+
+    def package(self):
+        base = self.source_folder
+        relative = "3rdparty/dawn"
+
+        # headers
+        # Expose only webgpu.h to RTC, at present
+        self.copy("webgpu.h", src=base + "/include/webgpu", dst=relative + "/include/webgpu")
+        self.copy("webgpu.h", src=base + "/out/gen/include/dawn", dst=relative + "/out/gen/include/dawn")
+
+        # libraries
+        output = "output/" + str(self.settings.platform_architecture_target) + "/staticlib"
+        self.copy("*" + self.name + "*", src=base + "/../../" + output, dst=output)
