f44e0c26b040c9b86d0f2288a6974bc23b7bdb69
julialubiszewska <julialubiszewska12@gmail.com>
2025-04-11 19:36:06 +0100

    mac scripting progress

--- file_splitter.py
+++ file_splitter.py
@@ -2,6 +2,7 @@ from itertools import groupby
 from os.path import dirname
 from os.path import commonpath
 from os import getcwd
+from pathlib import Path
 import re
 
 def file_to_list(file_path, dawn_lines, platform_lines):
@@ -15,8 +16,10 @@ def file_to_list(file_path, dawn_lines, platform_lines):
                 match = re.search(regex, line)
                 if(match != None) :
                     line = match.group(1)
-                    if "/metal/" in line or "/msl/" in line:
-                        platform_lines.append(line)
+                    if "/metal/" in line:
+                        platform_lines[0].append(line)
+                    elif "/msl/" in line:
+                        platform_lines[1].append(line)
                     else:
                         dawn_lines.append(line)
     except FileNotFoundError:
@@ -32,34 +35,10 @@ parents = {
     "src/tint/utils" : [],
 }
 
-paths = [
-  "src/dawn/native/SharedFence.cpp",
-  "src/dawn/native/SharedResourceMemory.cpp",
-  "src/dawn/native/SharedTextureMemory.cpp",
-  "src/dawn/native/Subresource.cpp",
-  "src/tint/lang/core/constant/node.cc",
-  "src/tint/lang/core/constant/scalar.cc",
-  "src/tint/lang/core/constant/splat.cc",
-  "src/dawn/common/FutureUtils.cpp",
-  "src/tint/lang/core/constant/value.cc",
-  "src/tint/lang/wgsl/ast/builder_rtc_shim1.cc",
-  "src/dawn/common/GPUInfo.cpp",
-  "src/dawn/common/Log.cpp",
-  "src/tint/lang/wgsl/ast/builtin_attribute.cc",
-  "src/tint/utils/generation_id.cc",
-  "src/tint/utils/macros/macros.cc",
-  "src/tint/utils/math/math_rtc_shim.cc",
-  "src/tint/utils/memory/memory.cc",
-  "src/tint/utils/reflection.cc",
-]
-
 file_path = "temp_file"
 paths = []
-platform_paths = []
-file_to_list(file_path, paths, platform_paths)
-
-sorted(paths)
-sorted(platform_paths)
+platform_lines = [[],[]]
+file_to_list(file_path, paths, platform_lines)
 
 out = {}
 others = []
@@ -87,20 +66,38 @@ for parent, files in parents.items():
         result = result + "  DAWN_GEN_OUTPUT_DIR..\"/src/dawn/native/**.cpp\",\n"
     if parent == "src/dawn/common":
         result = result + "  DAWN_GEN_OUTPUT_DIR..\"/src/dawn/common/**.cpp\",\n"
+    files.sort()
     for file in files:
         result = result + "  \"" + file + "\",\n"
     result = result + "\n"
 
+metal_str = ""
+platform_lines[0].sort()
+for file in platform_lines[0] :
+    metal_str = metal_str + " \"" + file + "\",\n"
+metal_str = metal_str + "\n"
+
+msl_str = ""
+platform_lines[1].sort()
+
+msl_str += "    -- CMake target: tint_lang_msl\n"
+nxt = False
+for file in platform_lines[1] :
+    if not nxt and "writer" in file:
+        msl_str += "\n    -- CMake target: tint_lang_msl_writer\n"
+        nxt = True
+    msl_str = msl_str + " \"" + file + "\",\n"
+msl_str = msl_str + "\n"
+
+
 with open ('dawn.lua', 'r' ) as fl:
     content = fl.read()
     content_new = re.sub('files {\n.*--.*?src/dawn/native/(.|\n)*?}', 'files {\n' + result + '}', content)
+    content_new = re.sub('(if \(enable_metal\) then(.|\n)*?files {\n)(.|\n)*?}', r'\1' + metal_str + '}', content_new)
+    content_new = re.sub('(if \(enable_msl\) then(.|\n)*?files {\n)(.|\n)*?}', r'\1' + msl_str + '}', content_new)
 with open('dawn.lua', 'w') as fl:
     fl.write(content_new)
 
-
-print()
-for line in platform_paths:
-    print("\"" + line + "\",")
 
 print()
 print()
--- test_script.sh
+++ test_script.sh
@@ -11,7 +11,7 @@
 cd ..
 mkdir temp_dawn_build
 cd temp_dawn_build
-/usr/local/rtc/cmake/3.29.2/bin/cmake ../dawn -DDAWN_BUILD_TESTS=OFF -DTINT_BUILD_TESTS=OFF -DDAWN_ENABLE_VULKAN=ON -DTINT_BUILD_GLSL_WRITER=OFF -DTINT_BUILD_GLSL_VALIDATOR=OFF -DDAWN_USE_GLFW=OFF -DDAWN_BUILD_SAMPLES=OFF -DPython3_EXECUTABLE=/usr/local/rtc/python/3.12/bin/python3.12 -DENABLE_NULL=OFF -DDAWN_FETCH_DEPENDENCIES=ON
+/usr/local/rtc/cmake/3.29.2/bin/cmake ../dawn -DDAWN_BUILD_TESTS=OFF -DTINT_BUILD_TESTS=OFF -DDAWN_ENABLE_VULKAN=OFF -DTINT_BUILD_GLSL_WRITER=OFF -DTINT_BUILD_GLSL_VALIDATOR=OFF -DDAWN_USE_GLFW=OFF -DDAWN_BUILD_SAMPLES=OFF -DPython3_EXECUTABLE=/usr/local/rtc/python/3.12/bin/python3.12 -DENABLE_NULL=OFF -DDAWN_FETCH_DEPENDENCIES=OFF
 cd ../dawn
 touch "temp_file"
 grep "file" ../temp_dawn_build/compile_commands.json > temp_file
