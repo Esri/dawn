6ecff7c71a61f926a3919a3dba64c075ae786a0a
julialubiszewska <julialubiszewska12@gmail.com>
2025-04-14 15:23:09 +0100

    t

--- shim_script.sh
+++ shim_script.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
 variable=`grep '\.c.*' dawn.lua | sed -n 's/^\ *\".*\/\([a-zA-Z0-9_]*\.[cp]*\)\"\,/\1/p' | sort | uniq -d`
-#var=`grep 'load\.c.*' dawn.lua | sed 's/\"\(.*\)load\.c.*\"\,/\1/'`
 
 while IFS= read -r line ; do 
   pattern="$line"
   sub="s/^\ *\"\(.*\)\/${line}\"\,/\1/p"
-  #echo $sub
-  echo ""
-  echo $line
-  echo "==============="
   var=`grep "$pattern" dawn.lua | sed -n "$sub"`
   int=0
-  #echo $var
   while IFS= read -r file ; do 
     if ((int > 0)); then
       search="\(.*\)\.\(.*\)"
       replace="bla"
       result="$(sed "s/\(.*\)\.\(.*\)/\1_rtc_shim_${int}.\2/" <<< $line)"
-      path="${file}/${result}"
-      echo $path
-      touch $path
-      echo "#include \"${line}\"" >> $path
+      shim_path="${file}/${result}"
+      old_path="${file}/${line}"
+      echo "#include \"${line}\"" >> $shim_path
+      echo "${old_path} ${shim_path}" >> "shim_temp_file.txt"
     fi
     int=$((int+1))
   done <<< "$var"
 done <<< "$variable"
+
+/usr/local/rtc/python/3.12/bin/python shim_replacer.py
+rm shim_temp_file.txt
