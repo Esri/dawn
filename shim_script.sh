e4a3dc9285f11a3172bee6fa4bc10881297f0627
julialubiszewska <julialubiszewska12@gmail.com>
2025-04-14 14:41:43 +0100

    temp shim file

--- shim_script.sh
+++ shim_script.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-variable=`grep '\.c.*' dawn.lua | sed 's/^\ *\".*\/\([a-zA-Z0-9_]*\)\.[cp]*\"\,/\1/' | sort | uniq -d`
-var=`grep 'load\.c.*' dawn.lua | sed 's/\"\(.*\)load\.c.*\"\,/\1/'`
-
-int=0
-while IFD= read -r line ; do
-  if (( int > 0)); then
-    echo $int
-    #path="${line}load_rtc_shim.cc"
-    #echo $path
-    #touch $path
-    #echo '#include "load.cc"' >> $path
-  fi
-  echo $line
-  int=$((int+1))
-
-done <<< "$var"
+variable=`grep '\.c.*' dawn.lua | sed -n 's/^\ *\".*\/\([a-zA-Z0-9_]*\.[cp]*\)\"\,/\1/p' | sort | uniq -d`
+#var=`grep 'load\.c.*' dawn.lua | sed 's/\"\(.*\)load\.c.*\"\,/\1/'`
 
 while IFS= read -r line ; do 
-  #echo "$line"
-  pattern="$line\.c.*"
-  sub="s/\"\(.*\)${line}\.c.*\"\,/\1/"
+  pattern="$line"
+  sub="s/^\ *\"\(.*\)\/${line}\"\,/\1/p"
   #echo $sub
-  var=`grep "$pattern" dawn.lua | sed "$sub"`
+  echo ""
+  echo $line
+  echo "==============="
+  var=`grep "$pattern" dawn.lua | sed -n "$sub"`
   int=0
   #echo $var
   while IFS= read -r file ; do 
-    echo $file
+    if ((int > 0)); then
+      search="\(.*\)\.\(.*\)"
+      replace="bla"
+      result="$(sed "s/\(.*\)\.\(.*\)/\1_rtc_shim_${int}.\2/" <<< $line)"
+      path="${file}/${result}"
+      echo $path
+      touch $path
+      echo "#include \"${line}\"" >> $path
+    fi
+    int=$((int+1))
   done <<< "$var"
 done <<< "$variable"
