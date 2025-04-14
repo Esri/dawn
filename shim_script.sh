8627b356244b15cc35fbd1aa17f2eb38990c1e0a
julialubiszewska <julialubiszewska12@gmail.com>
2025-04-14 13:15:45 +0100

    t

--- shim_script.sh
+++ shim_script.sh
@@ -7,22 +7,25 @@ int=0
 while IFD= read -r line ; do
   if (( int > 0)); then
     echo $int
-    path="${line}load_rtc_shim.cc"
-    echo $path
-    touch $path
-    echo '#include "load.cc"' >> $path
+    #path="${line}load_rtc_shim.cc"
+    #echo $path
+    #touch $path
+    #echo '#include "load.cc"' >> $path
   fi
   echo $line
   int=$((int+1))
 
 done <<< "$var"
 
-#while IFS= read -r line ; do 
-#  echo "$line bla ba"
-#  pattern="$line\.c.*"
-#  var=`grep "load\.c.*" dawn.lua`
-#  echo $var
-#  #grep "$pattern" dawn.lua | while read -r file; do
-#  #  echo $file
-#  #done
-#done <<< "$variable"
+while IFS= read -r line ; do 
+  #echo "$line"
+  pattern="$line\.c.*"
+  sub="s/\"\(.*\)${line}\.c.*\"\,/\1/"
+  #echo $sub
+  var=`grep "$pattern" dawn.lua | sed "$sub"`
+  int=0
+  #echo $var
+  while IFS= read -r file ; do 
+    echo $file
+  done <<< "$var"
+done <<< "$variable"
