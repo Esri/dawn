7666f89b4ec519c5f50c15d84ec09c93d9207d91
julialubiszewska <julialubiszewska12@gmail.com>
2025-04-25 10:23:54 +0100

    script fix

--- shim_script.sh
+++ shim_script.sh
@@ -7,15 +7,15 @@ repeated_names=`grep '\.c.*' dawn.lua | sed -n 's/^\ *\".*\/\([a-zA-Z0-9_]*\)\.[
 
 while IFS= read -r file_name ; do 
   int=0
-  replace_pattern="s/^\ *\"\(.*\/\)${file_name}\(\.[cp]*\)\"\,/\1 \2/pI"
+  replace_pattern="s/^\ *\"\(.*\/\)\(${file_name}\)\(\.[cp]*\)\"\,/\1 \2 \3/pI"
   files_to_replace=`grep -i "\/${file_name}\." dawn.lua | sed -n "$replace_pattern"`
 
   while IFS= read -r line ; do
-    IFS=' ' read -r path extension <<< "$line"
+    IFS=' ' read -r path name extension <<< "$line"
     if ((int > 0)); then
-      replace_pattern2="s|^\(\ *\"$path$file_name\)\([cp]*.*\)|\1_rtc_shim_$int\2|I"
+      replace_pattern2="s|^\(\ *\"$path$name\)\(\.[cp]*\"\,\)|\1_rtc_shim_$int\2|I"
       _sed_inplace "$replace_pattern2" dawn.lua
-      echo "#include \"${path}${file_name}${extension}\"" >> "${path}${file_name}_rtc_shim_${int}${extension}"
+      echo "#include \"${name}${extension}\"" >> "${path}${name}_rtc_shim_${int}${extension}"
     fi
     int=$((int+1))
   done <<< "$files_to_replace"
