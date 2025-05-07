688082bbe8cdfcb29cf51dbd924c7897e4916183
julialubiszewska <julialubiszewska12@gmail.com>
2025-05-07 16:05:43 +0100

    Add update scripts

---
+++ dawn_update_tools/variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python_version="3.12"
+cmake_version="3.29.2"
+llvm_version="19.1.2"
+windows_target_sdk="10.0.19041.0"
+
+visual_studio_install_paths=(\
+    "C:/Program Files/Microsoft Visual Studio/2022/Enterprise" \
+    "C:/Program Files/Microsoft Visual Studio/2022/Professional" \
+    "C:/Program Files/Microsoft Visual Studio/2022/Preview" \
+    "C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools" \
+  )
